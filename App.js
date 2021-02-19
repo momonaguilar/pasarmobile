27b0b38a64942ca8a278562b8973ab206b7d610c
Reymond Aguilar <jose.ra@jirnexu.com>
2021-02-19 23:09:50 +0800

    Minor code updates; spacing only

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import React from "react";
-
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-
 import SplashScreen from "./src/screens/SplashScreen";
 import LoginScreen from "./src/screens/LoginScreen";
 import RegisterScreen from "./src/screens/RegisterScreen";
